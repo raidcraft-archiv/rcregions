e8f03dd501411b452444ad30311efae6a31c694b
Silthus <md.doering@yahoo.de>
2012-01-02 16:12:27 +0100

    made it an OR statement instead of an AND statement

--- src/main/java/de/raidcraft/rcregions/commands/RegionCommand.java
+++ src/main/java/de/raidcraft/rcregions/commands/RegionCommand.java
@@ -1,5 +1,6 @@
 package de.raidcraft.rcregions.commands;
 
+import com.silthus.raidcraft.util.RCCommandManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,9 +12,17 @@ import org.bukkit.command.CommandSender;
  */
 public class RegionCommand implements CommandExecutor {
 
+    private final RCCommandManager cmd = new RCCommandManager();
+    private CommandSender sender;
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
-        return false;  // TODO: implement
+    public boolean onCommand(CommandSender sender, Command command, String cmdLabel, String[] args) {
+        String label;
+        this.sender = sender;
+        if (args.length > 0) {
+            label = args[0];
+
+            if (cmd.is(label, "buy", ""))
+        }
     }
 }
