b9ca3c9c156e3460c17a051b206ab341390dd7a8
Dragonfire <bastian616-git@yahoo.de>
2014-08-08 03:29:27 +0200

    forgot the events

--- src/main/java/com/raidcraft/rcregions/api/raidcraftevents/RcPlayerEntryRegionEvent.java
+++ src/main/java/com/raidcraft/rcregions/api/raidcraftevents/RcPlayerEntryRegionEvent.java
@@ -3,6 +3,7 @@ package com.raidcraft.rcregions.api.raidcraftevents;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
@@ -11,22 +12,20 @@ import org.bukkit.event.HandlerList;
 /**
  * @author Dragonfire
  */
+@Setter
+@Getter
 public class RcPlayerEntryRegionEvent extends Event implements Cancellable {
 
-    @Setter
-    @Getter
     boolean cancelled = false;
-    @Setter
-    @Getter
     private Player player;
-    @Setter
-    @Getter
     private ProtectedRegion region;
+    private Location lastLocation;
 
-    public RcPlayerEntryRegionEvent(Player player, ProtectedRegion region) {
+    public RcPlayerEntryRegionEvent(Player player, ProtectedRegion region, Location lastLocation) {
 
         this.player = player;
         this.region = region;
+        this.lastLocation = lastLocation;
     }
 
     // Bukkit stuff
